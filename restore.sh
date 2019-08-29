8d5316188719065b5d31f0e699ce744adb27646a
gryphon <mail@grigor.io>
2019-08-29 12:41:26 +0300

    Fix restore

--- restore.sh
+++ restore.sh
@@ -20,7 +20,7 @@ export B2_ACCOUNT_KEY=$(cat /run/secrets/b2_application_key 2>/dev/null);
 export AWS_ACCESS_KEY_ID=$(cat /run/secrets/s3_key 2>/dev/null)
 export AWS_SECRET_ACCESS_KEY=$(cat /run/secrets/s3_secret 2>/dev/null)
 
-export RESTIC_REPOSITORY=b2:$(cat /run/secrets/backup_bucket);
+export RESTIC_REPOSITORY=$(cat /run/secrets/backup_repository);
 export RESTIC_PASSWORD=$(cat /run/secrets/backup_password);
 
 echo "Restoring ${snapshot} to /backup/restored"
